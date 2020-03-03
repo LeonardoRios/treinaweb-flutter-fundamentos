66d3882048879e670ec1c8d16561c5419cab3e28
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2020-03-03 09:38:02 -0300

    05.01_Agrupando_Widgets_Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -8,47 +8,50 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Icon(
-            Icons.monetization_on,
-            size: 120,
-            color: Colors.blue,
-          ),
-          TextFormField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Quantidade de pessoas",
-                labelStyle: TextStyle(color: Colors.blue)
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.blue, fontSize: 18),
-          ),
-          TextFormField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: "Valor da conta",
-                labelStyle: TextStyle(color: Colors.blue)
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.blue, fontSize: 18),
-          ),
-          RaisedButton(
-            onPressed: () {},
-            color: Colors.blue,
-            child: Text(
-              "Calcular"
-            ),
-          ),
-          Text(
-            "Valor por pessoa",
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.blue, fontSize: 18),
-          )
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Raxa Conta"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          child: (Column(
+            children: <Widget>[
+              Icon(
+                Icons.monetization_on,
+                size: 120,
+                color: Colors.blue,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Quantidade de pessoas",
+                    labelStyle: TextStyle(color: Colors.blue)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.blue, fontSize: 18),
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Valor da conta",
+                    labelStyle: TextStyle(color: Colors.blue)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.blue, fontSize: 18),
+              ),
+              RaisedButton(
+                onPressed: () {},
+                color: Colors.blue,
+                child: Text("Calcular"),
+              ),
+              Text(
+                "Valor por pessoa",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.blue, fontSize: 18),
+              )
+            ],
+          )),
+        ),
       ),
     );
   }
-
 }
