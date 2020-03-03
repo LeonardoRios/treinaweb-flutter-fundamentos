379493660c1be2bfed945ca5fd4dc50570c393e0
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2020-03-03 09:18:11 -0300

    04.03_Estrutura_Hierarquica_Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -8,44 +8,47 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Icon(
+            Icons.monetization_on,
+            size: 120,
+            color: Colors.blue,
+          ),
+          TextFormField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Quantidade de pessoas",
+                labelStyle: TextStyle(color: Colors.blue)
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.blue, fontSize: 18),
+          ),
+          TextFormField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: "Valor da conta",
+                labelStyle: TextStyle(color: Colors.blue)
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.blue, fontSize: 18),
+          ),
+          RaisedButton(
+            onPressed: () {},
+            color: Colors.blue,
+            child: Text(
+              "Calcular"
+            ),
+          ),
+          Text(
+            "Valor por pessoa",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.blue, fontSize: 18),
+          )
+        ],
+      ),
+    );
   }
-
-  Icon icon = Icon(
-    Icons.monetization_on,
-    size: 120,
-    color: Colors.blue,
-  );
-
-  TextFormField formPessoas = TextFormField(
-    keyboardType: TextInputType.number,
-    decoration: InputDecoration(
-      labelText: "Quantidade de pessoas",
-      labelStyle: TextStyle(color: Colors.blue)
-    ),
-    textAlign: TextAlign.center,
-    style: TextStyle(color: Colors.blue, fontSize: 18),
-  );
-
-  TextFormField formConta = TextFormField(
-    keyboardType: TextInputType.number,
-    decoration: InputDecoration(
-        labelText: "Valor da conta",
-        labelStyle: TextStyle(color: Colors.blue)
-    ),
-    textAlign: TextAlign.center,
-    style: TextStyle(color: Colors.blue, fontSize: 18),
-  );
-
-  RaisedButton botaoCalcular = RaisedButton(
-    onPressed: () {},
-    color: Colors.blue,
-  );
-
-  Text textoValorPessoa = Text(
-    "Valor por pessoa",
-    textAlign: TextAlign.center,
-    style: TextStyle(color: Colors.blue, fontSize: 18),
-  );
 
 }
