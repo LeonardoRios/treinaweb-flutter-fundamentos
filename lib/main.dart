804a233585dd9b362c937e4e8bd1eef9119aa70d
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2020-03-03 09:08:13 -0300

    04.02_Criando_Formulario_Aplicacao

--- lib/main.dart
+++ lib/main.dart
@@ -10,4 +10,42 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Container();
   }
+
+  Icon icon = Icon(
+    Icons.monetization_on,
+    size: 120,
+    color: Colors.blue,
+  );
+
+  TextFormField formPessoas = TextFormField(
+    keyboardType: TextInputType.number,
+    decoration: InputDecoration(
+      labelText: "Quantidade de pessoas",
+      labelStyle: TextStyle(color: Colors.blue)
+    ),
+    textAlign: TextAlign.center,
+    style: TextStyle(color: Colors.blue, fontSize: 18),
+  );
+
+  TextFormField formConta = TextFormField(
+    keyboardType: TextInputType.number,
+    decoration: InputDecoration(
+        labelText: "Valor da conta",
+        labelStyle: TextStyle(color: Colors.blue)
+    ),
+    textAlign: TextAlign.center,
+    style: TextStyle(color: Colors.blue, fontSize: 18),
+  );
+
+  RaisedButton botaoCalcular = RaisedButton(
+    onPressed: () {},
+    color: Colors.blue,
+  );
+
+  Text textoValorPessoa = Text(
+    "Valor por pessoa",
+    textAlign: TextAlign.center,
+    style: TextStyle(color: Colors.blue, fontSize: 18),
+  );
+
 }
